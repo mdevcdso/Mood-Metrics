ab8fc91450a5270bb3ea87b38b16e6c8b0a1ce58
Marion CANDUSSO <marioncandusso@gmail.com>
2026-04-12 19:15:20 +0200

    feat: ajout des méthodes dans SettingsDataSource

--- lib/repositories/settings_repository/settings_data_source/settings_data_source.dart
+++ lib/repositories/settings_repository/settings_data_source/settings_data_source.dart
@@ -1,3 +1,9 @@
 abstract class SettingsDataSource {
-
+  Future<int> getReminderHour();
+  Future<int> getReminderMinute();
+  Future<void> setReminderTime(int hour, int minute);
+  Future<bool> isReminderEnabled();
+  Future<void> setReminderEnabled(bool enabled);
+  Future<String> getThemeMode();
+  Future<void> setThemeMode(String mode);
 }
