5578d757ed72b6090598a683b94cd79673ee3932
Marion CANDUSSO <marioncandusso@gmail.com>
2026-04-13 21:51:13 +0200

    feat: ajout de la navigation

---
+++ lib/screens/home_screen.dart
@@ -0,0 +1,63 @@
+import 'package:flutter/material.dart';
+import 'package:mood_metrics/screens/analytics_screen.dart';
+import 'package:mood_metrics/screens/journal_screen.dart';
+import 'package:mood_metrics/screens/settings_screen.dart';
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  int _currentIndex = 0;
+
+  final _screens = const [
+    JournalScreen(),
+    AnalyticsScreen(),
+    SettingsScreen(),
+  ];
+
+  final _titles = const [
+    'Journal',
+    'Analyse',
+    'Paramètres',
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(_titles[_currentIndex]),
+        centerTitle: true,
+      ),
+      body: _screens[_currentIndex],
+      bottomNavigationBar: NavigationBar(
+        selectedIndex: _currentIndex,
+        onDestinationSelected: (index) {
+          setState(() {
+            _currentIndex = index;
+          });
+        },
+        destinations: const [
+          NavigationDestination(
+            icon: Icon(Icons.book_outlined),
+            selectedIcon: Icon(Icons.book),
+            label: 'Journal',
+          ),
+          NavigationDestination(
+            icon: Icon(Icons.bar_chart_outlined),
+            selectedIcon: Icon(Icons.bar_chart),
+            label: 'Analyse',
+          ),
+          NavigationDestination(
+            icon: Icon(Icons.settings_outlined),
+            selectedIcon: Icon(Icons.settings),
+            label: 'Paramètres',
+          ),
+        ],
+      ),
+    );
+  }
+}
