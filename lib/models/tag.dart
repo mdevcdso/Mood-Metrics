1c7a0007f85f8f722dcfa9750aa472f8df172048
Marion CANDUSSO <marioncandusso@gmail.com>
2026-04-16 00:37:16 +0200

    feat: ajout modele tag

---
+++ lib/models/tag.dart
@@ -0,0 +1,13 @@
+enum Tag {
+  sport('Sport'),
+  alcohol('Alcool'),
+  hardWork('Travail intense'),
+  family('Famille'),
+  stress('Stress'),
+  outing('Sortie'),
+  badSleep('Mauvais sommeil');
+
+  final String label;
+
+  const Tag(this.label);
+}
