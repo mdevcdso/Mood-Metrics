d17791a15a5ba807aa877b02301b347b48477ca9
Marion CANDUSSO <marioncandusso@gmail.com>
2026-04-16 00:38:53 +0200

    feat: ajout interface JournalDataSource

---
+++ lib/repositories/journal_repository/journal_data_source/journal_data_source.dart
@@ -0,0 +1,13 @@
+import 'package:mood_metrics/models/journal_entry.dart';
+
+abstract class JournalDataSource {
+  Stream<List<JournalEntry>> watchEntries();
+
+  Future<JournalEntry?> getEntryByDate(DateTime date);
+
+  Future<void> addEntry(JournalEntry entry);
+
+  Future<void> updateEntry(JournalEntry entry);
+
+  Future<void> deleteEntry(int id);
+}
